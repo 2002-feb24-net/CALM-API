83711b356d07f333470f490678c4d3c16e629fd5
MishaelHarrison <mishael.harrison@gmail.com>
2020-05-02 09:28:01 -0500

    added a small amount of seed data

---
+++ Calm.App/Seeder.cs
@@ -0,0 +1,23 @@
+﻿using Calm.Dtb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calm.App
+{
+    public class Seeder
+    {
+        public static void Seed(CalmContext context)
+        {
+            if (context.Users.Count() == 0)
+            {
+                context.Users.AddRange(
+                    new User() { FName = "admin", LName = "user", Username = "admin", Password = "admin", IsAdmin = true },
+                    new User() { FName = "timmy", LName = "jimmy", Username = "TimmyJ", Password = "TimTim", IsAdmin = true });
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
