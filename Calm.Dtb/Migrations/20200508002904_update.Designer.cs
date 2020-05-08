db14e4c113247e1c649470df3dcbe0135e5af020
Mishael Harrison <Mishael.Harrison@gmail.com>
2020-05-07 19:30:29 -0500

    added missing migrations

--- Calm.Dtb/Migrations/20200508002904_update.Designer.cs
+++ Calm.Dtb/Migrations/20200508002904_update.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Calm.Dtb;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Calm.Dtb.Migrations
 {
     [DbContext(typeof(CalmContext))]
-    partial class CalmContextModelSnapshot : ModelSnapshot
+    [Migration("20200508002904_update")]
+    partial class update
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
