5a38e58080b06914daa518efac60a2bb1859a5c5
Mishael Harrison <Mishael.Harrison@gmail.com>
2020-05-13 22:16:52 -0500

    more code smells

--- Calm.Lib/Delete.cs
+++ Calm.Lib/Delete.cs
@@ -26,27 +26,6 @@ namespace Calm.Lib
             await Input.Remove(item);
         }
 
-        public async Task RemoveGathering(string username, string password, string title)
-        {
-            int id = (await Logic.Login(Output, username, password)).Id;
-            var item = await Output.GetFind<Gathering>(x=> x.Title == title);
-
-            if (item == null)
-            {
-                throw new Exception("404", new Exception("Gathering Title is incorect"));
-            }
-            if (!(item.organizerId == id || null != await Output.GetFind<AdminInfo>(x=> x.SuperAdmin && x.userId == id)))
-            {
-                throw new Exception("403", new Exception("User is not the gathering organiser"));
-            }
-            foreach (var tag in await Output.GetFilter<Link>(x=> x.gatheringId == item.id))
-            {
-                await Input.Remove(tag);
-            }
-
-            await Input.Remove(item);
-        }
-
         public async Task RemoveUser(string username, string password, string subjectUser)
         {
             var users = new User[2];
@@ -80,6 +59,27 @@ namespace Calm.Lib
             {
                 await Input.Remove(users[1]);
             }
+        }
+
+        public async Task RemoveGathering(string username, string password, string title)
+        {
+            int id = (await Logic.Login(Output, username, password)).Id;
+            var item = await Output.GetFind<Gathering>(x=> x.Title == title);
+
+            if (item == null)
+            {
+                throw new Exception("404", new Exception("Gathering Title is incorect"));
+            }
+            if (!(item.organizerId == id || null != await Output.GetFind<AdminInfo>(x=> x.SuperAdmin && x.userId == id)))
+            {
+                throw new Exception("403", new Exception("User is not the gathering organiser"));
+            }
+            foreach (var tag in await Output.GetFilter<Link>(x=> x.gatheringId == item.id))
+            {
+                await Input.Remove(tag);
+            }
+
+            await Input.Remove(item);
         }
     }
 }
--- Calm.Lib/Interfaces/IDelete.cs
+++ Calm.Lib/Interfaces/IDelete.cs
@@ -10,16 +10,16 @@ namespace Calm.Lib
         /// <param name="username">username of user</param>
         Task RemoveUser(string username, string password);
         /// <summary>
-        /// removes a gathering
-        /// </summary>
-        /// <param name="username">username of user running request</param>
-        /// <param name="title">title of gathering</param>
-        Task RemoveGathering(string username, string password, string title);
-        /// <summary>
         /// removes a user of lower rank than the one logged in
         /// </summary>
         /// <param name="username">username of logged in user</param>
         /// <param name="subjectUser">username of user to remove</param>
         Task RemoveUser(string username, string password, string subjectUser);
+        /// <summary>
+        /// removes a gathering
+        /// </summary>
+        /// <param name="username">username of user running request</param>
+        /// <param name="title">title of gathering</param>
+        Task RemoveGathering(string username, string password, string title);
     }
 }
